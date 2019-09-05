6ce25bc384c453869facf3c0c1d1495b28d56943
flurt <azriantar@outlook.com>
2019-09-05 12:20:42 +0100

    changing font to Roboto Mono

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:pixelsize=18:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Roboto Mono for Powerline:size=11:antialias=true:autohint=true";
+static int borderpx = 1;
 
 /*
  * What program is execed by st depends of these precedence rules:
